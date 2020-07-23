14460c534019091ee5c360b9da541a48c5c35867
Kevin <ruan8111@gmail.com>
2020-07-22 20:58:48 -0400

    router

--- extension/frontend/view/Components/Navbar.js
+++ extension/frontend/view/Components/Navbar.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './About'
+import Users from './Users'
+import Atoms from '../atomView'
 
-const Navbar = () => (
-  <header className='navbar'>
-    <div className='navbar__title navbar__item'>Cutco</div>
-    <div className='navbar__item'>About Us</div>
-    <div className='navbar__item'>Contact</div>
-    <div className='navbar__item'>Help</div>
-  </header>
+const Navbar = (props) => (
+  <Router>
+    <header className='navbar'>
+      <Link to="/tab1"><div className='navbaritem'>Tab 1</div></Link>
+      <Link to="/tab2"><div className='navbaritem'>Tab 2</div></Link>
+      <Switch>
+        <Route path="/tab1">
+          <Atoms tree={props.tree} />
+        </Route>
+        <Route path="/tab2">
+          <Users />
+        </Route>
+      </Switch>
+    </header>
+  </Router>
 );
 
 export default Navbar;
