4a384997d2283a2ab5f686e23352a84f00d4ca68
Kevin <ruan8111@gmail.com>
2020-07-30 15:57:31 -0400

    conditional rendering for graph

--- extension/frontend/view/Components/GraphPanel.js
+++ extension/frontend/view/Components/GraphPanel.js
@@ -1,33 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  Redirect
 } from "react-router-dom";
 
 import SelectorTree from '../SelectorTree'
 import TreeView from '../TreeView';
 
 
-const GraphPanel = (props) => (
+const GraphPanel = (props) => {
+  const [view, setView] = useState(true);
+  const [viewName, setviewName] = useState('Component Tree')
 
-  <Router>
-    <div className='graphDisplays'>
-      <div id='graph-options'>
-        <Link to="/"><div className='componentTreeBtn'>Component Tree</div></Link>
-        <Link to="/stateTree"><div className='stateTreeBtn'>Selectors and Atoms</div></Link>
-      </div>
-      <Switch>
-        <Route path="/stateTree">
-          <SelectorTree tree={props.tree} />
-        </Route>
-        <Route path="/">
-          <TreeView tree={props.tree} />
-        </Route>
-      </Switch>
+  const changeView = () => {
+    setView(!view);
+    // change text of the button
+    // buttonName === 'Component Tree' ? 
+    if (viewName === 'Component Tree') {
+      setviewName('Selector Tree')
+    } else {
+      setviewName('Component Tree')
+    }
+
+    // console.log(view);
+  }
+
+  return (
+    <div className='graph-container'>
+      <h1 className='h1-absolute' >{viewName}</h1>
+      <button onClick={changeView} className='graph-button-1'>Change View</button>
+      {view ? <TreeView tree={props.tree} /> : <SelectorTree tree={props.tree} />}
     </div>
-  </Router>
-);
+
+    // <Router>
+    //   <div className='graphDisplays'>
+    //     <div id='graph-options'>
+    //       <Link to="/"><button onClick={changeView} className='graph-button-1'>Component Tree</button></Link>
+    //       <Link to="/stateTree"><button className='graph-button-2'>Selectors and Atoms</button></Link>
+    //     </div>
+    //     <Switch>
+    //       <Route
+    //         exact
+    //         path="/"
+    //         render={() => {
+    //           console.log(view)
+    //           return (
+    //             view ? <Redirect to="/" /> : <Redirect to="/stateTree" />
+    //           )
+    //         }
+    //         }
+    //       >
+    //       </Route>
+
+    //       <Route path="/stateTree"><SelectorTree tree={props.tree} /></Route>
+    //       <Route path="/"><TreeView tree={props.tree} /></Route>
+    //     </Switch>
+    //   </div>
+    // </Router>
+  )
+};
 
 export default GraphPanel;
